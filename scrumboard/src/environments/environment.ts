17ab25b0bdd3ba2e0fc97e88cd16bef95035dea6
Michiel Buijserd <mbuyserd@gmail.com>
2022-05-25 17:47:17 +0200

    added firebass

--- scrumboard/src/environments/environment.ts
+++ scrumboard/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'scrum-board-herd',
+    appId: '1:1041158042749:web:86177e692ca6ab50a4ad14',
+    storageBucket: 'scrum-board-herd.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAN5FWvyWWaos83cQI3oh1dhZke2SvUR3A',
+    authDomain: 'scrum-board-herd.firebaseapp.com',
+    messagingSenderId: '1041158042749',
+  },
   production: false
 };
 
