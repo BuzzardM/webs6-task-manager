9c979d253019497802d250254f92def113e55ee9
Michiel Buijserd <mbuyserd@gmail.com>
2022-06-01 16:01:49 +0200

    added auth routing

--- scrumboard/src/app/app-routing.module.ts
+++ scrumboard/src/app/app-routing.module.ts
@@ -4,17 +4,35 @@ import {SignInComponent} from "./components/auth/sign-in/sign-in.component";
 import {SignUpComponent} from "./components/auth/sign-up/sign-up.component";
 import {HomeComponent} from "./components/home/home.component";
 import {ProjectsComponent} from "./components/projects/projects.component";
-import {AuthGuard, redirectUnauthorizedTo} from "@angular/fire/auth-guard";
+import {canActivate, redirectLoggedInTo, redirectUnauthorizedTo} from "@angular/fire/auth-guard";
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'login', component: SignInComponent},
-  {path: 'register', component: SignUpComponent},
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: SignInComponent,
+    ...canActivate(redirectLoggedInToHome)
+  },
+  {
+    path: 'register',
+    component: SignUpComponent,
+    ...canActivate(redirectLoggedInToHome)
+  },
   {
     path: 'projects',
     component: ProjectsComponent,
-    canActivate: [AuthGuard],
-    data: {authGuardPipe: () => redirectUnauthorizedTo(['login'])}
+    ...canActivate(redirectUnauthorizedToLogin)
   }
 ];
 
